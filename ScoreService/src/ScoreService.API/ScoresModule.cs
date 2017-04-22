3e424d7c9c75b52368f6d9f61f2906d75c987eab
Krzysztof Owsiany <gocom@o2.pl>
2017-04-22 11:24:42 +0200

    Add post map

--- ScoreService/src/ScoreService.API/ScoresModule.cs
+++ ScoreService/src/ScoreService.API/ScoresModule.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace ScoreService.API
 {
@@ -7,12 +9,27 @@ namespace ScoreService.API
     {
         public Task GetScore(HttpContext httpContext)
         {
-            return httpContext.Response.WriteAsync("po zmiana dddanie scoremodules ");
+            var result = DeserializeAsync<ApplicantRequest>(httpContext);
+
+            return httpContext.Response.WriteAsync($" {result.Name} po zmiana dddanie scoremodules ");
         }
 
-
+        public T DeserializeAsync<T>(HttpContext context) where T : class
+        {
+            if (context.Request.Body.CanRead)
+            {
+                using (var sr = new StreamReader(context.Request.Body))
+                using (var jr = new JsonTextReader(sr))
+                {
+                    return new JsonSerializer().Deserialize<T>(jr);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
-
 
     public class ApplicantRequest
     {
